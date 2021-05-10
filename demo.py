3a7795279199a02304aeebcf5abf109122df6245
Robert Ronan <37887237+rlronan@users.noreply.github.com>
2021-05-10 14:24:14 -0400

    added updated files

--- demo.py
+++ demo.py
@@ -14,8 +14,10 @@ from feature_vis import log_conv_features_callback
 from feature_vis import *
 from visualize_layers import VisualizeVggFeatures
 
-#%%
-
+#%% IMPORT DATA:
+##
+##
+##
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
 # Rescale the images from [0,255] to the [0.0,1.0] range.
@@ -28,21 +30,26 @@ x_test = tf.convert_to_tensor(x_test, dtype=tf.float32)
 y_train = tf.convert_to_tensor(y_train, dtype=tf.float32)
 y_test = tf.convert_to_tensor(y_test, dtype=tf.float32)
 
-
+#%% DEFINE LOGGING:
+##
+##
+##
 log_dir = pathlib.Path("./logs/run1/")
 if not os.path.exists(log_dir.parent): os.mkdir(log_dir.parent)
 if not os.path.exists(log_dir): os.mkdir(log_dir)
 
 
-file_writer = tf.summary.create_file_writer(log_dir)
+file_writer = tf.summary.create_file_writer("./logs/run1/")
 file_writer.set_as_default()
 
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1,
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="./logs/run1/", histogram_freq=1,
                                                       write_graph=False, write_images=False,
                                                       update_freq='epoch', profile_batch=0,
                                                       embeddings_freq=0, embeddings_metadata=None)
 tf.summary.experimental.set_step(0)
 
+##
+#%% DEFINE MODEL:
 
 inputs = tf.keras.Input(shape=(32,32,3), name='inputs') # do not include batch size
 
@@ -71,17 +78,17 @@ model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=Tru
               metrics=['accuracy'])
 
 model.summary()
-#%%
+#%% SHOW CONV LAYERS:
 # Show conv layers for callback:
 print("Showing the convolutional layers of the model: ")
 
 show_conv_layers(model=model)
 
 
-#%%
+#%% DEFINTE FEATURE CALLBACK:
 
 feature_callback = log_conv_features_callback(
-            log_dir=log_dir,
+            log_dir=pathlib.Path("./logs/run1/"),
             update_freq='epoch',
             update_freq_val=1,
             layer_nums=[0,3],
@@ -98,7 +105,9 @@ feature_callback = log_conv_features_callback(
 
 layer_feature_callback = VisualizeVggFeatures(sample_image=None)
 
-#%%
+#%% TRAIN MODEL
+##
+##
 model.fit(x_train,
           y_train,
           batch_size=1024,
